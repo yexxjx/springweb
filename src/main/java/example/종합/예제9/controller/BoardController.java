c3ce16afce06a0d8452bd7b750faa0841163eb0f
yexxjx <chenstyle02@gmail.com>
2026-02-27 11:46:46 +0900

    260227

--- src/main/java/example/종합/예제9/controller/BoardController.java
+++ src/main/java/example/종합/예제9/controller/BoardController.java
@@ -1,12 +1,40 @@
 package example.종합.예제9.controller;
 
 import example.종합.예제9.model.dao.BoardDao;
+import example.종합.예제9.model.dto.BoardDto;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController // 빈 등록+HTTP 기능+HTTP 응답 객체
 // component+responsebody
 public class BoardController {
+    @Autowired
+    private BoardDao boardDao;
+    @GetMapping("/board") // localhost:8080/board      // 1. 해당 메소드의 HTTP 메소드와 주소 정의
+    public List<BoardDto> findAll(){                   // 2. 매개변수 정의 *현재 없음
+        List<BoardDto> result=boardDao.findAll();      // 3. DAO 호출하여 결과 받기
+        return result;                                 // 4. DAO 결과로 응답하기
+    }
 
-    @Autowired private BoardDao boardDao;
+    @PostMapping("/board")
+    public boolean write(@RequestBody BoardDto boardDto) {
+        boolean result = boardDao.write(boardDto);
+        return result;
+    }
+
+    @PutMapping("/board")
+    public boolean update(@RequestBody BoardDto boardDto){
+        boolean result = boardDao.update(boardDto);
+        return result;
+    }
+
+    @DeleteMapping("/board")
+    public boolean delete(@RequestParam int bno){
+        boolean result = boardDao.delete(bno);
+        return result;
+    }
+
+
 }
--- src/main/java/example/종합/예제9/model/dao/BoardDao.java
+++ src/main/java/example/종합/예제9/model/dao/BoardDao.java
@@ -1,9 +1,16 @@
 package example.종합.예제9.model.dao;
 
+import example.종합.예제9.model.dto.BoardDto;
 import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ExecutionException;
 
 @Component // 빈 등록
 public class BoardDao {
@@ -22,5 +29,65 @@ public class BoardDao {
             System.out.println("[준비] 데이터베이스 연동 성공");
         }catch(Exception e){
             System.out.println("[경고] 데이터베이스 연동 실패 : 관리자에게 문의");}
+    }
+
+    // 1) 전체 조회
+    public List<BoardDto> findAll() {
+        List<BoardDto> list = new ArrayList<>();                                             // 1) 조회 결과 레코드들을 DTO로 저장하기 위한 리스트 선언
+        try {
+            String sql = "select*from board";                                                // 2) SQL 선언
+            PreparedStatement ps = conn.prepareStatement(sql);                               // 3) SQL 등록
+            ResultSet rs = ps.executeQuery();                                                // 4) SQL 실행하고 결과 받기
+            while (rs.next()) {                                                              // 5) 첫번째 레코드부터 마지막 레코드까지 반복
+                BoardDto boardDto = new BoardDto(rs.getInt("bno"), rs.getString("bcontent"), // 6) 조회 중인 레코드의 속성 정보들을 DTO 구성
+                        rs.getString("bwriter"), rs.getString("bdate"));
+                list.add(boardDto);                                                          // 7) 구성한 DTO를 리스트 저장
+            }
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+        return list;                                                                         // 8) 리스트 반환
+    }
+
+    // 2) 게시물 등록
+    public boolean write(BoardDto boardDto) {
+        try {
+            String sql = "insert into board(bcontent, bwriter) values(?, ?)"; // 1) SQL 작성
+            PreparedStatement ps=conn.prepareStatement(sql);                  // 2) SQL 등록
+            ps.setString(1, boardDto.getBcontent());                          // 3) SQL 첫번째 매개변수?에 값 대입한다.
+            ps.setString(2, boardDto.getBwriter());                           // 3) SQL 두번째 매개변수?에 값 대입한다.
+            int count=ps.executeUpdate();                                     // 4) SQL 실행하고 반영한 레코드 수 저장한다.
+            if(count==1) return true;                                         // 5) 반영한 레코드수가 1개이면 성공
+        } catch (Exception e) {
+            System.out.println(e);
+        } return false;                                                       // 6) 아니면 실패
+    }
+
+    // 3) 게시물 수정
+    public boolean update(BoardDto boardDto){
+        try{
+            String sql= "update board set bcontent=? where bno=?";
+            PreparedStatement ps=conn.prepareStatement(sql);
+            ps.setString(1, boardDto.getBcontent());
+            ps.setInt(2, boardDto.getBno());
+            int count=ps.executeUpdate();
+            if(count==1){return true;}
+        } catch (Exception e){
+            System.out.println(e);
+        } return false;
+    }
+
+    // 4. 게시물 삭제
+    public boolean delete(int bno) {
+        try {
+            String sql="delete from board where bno=?";
+            PreparedStatement ps=conn.prepareStatement(sql);
+            ps.setInt(1, bno);
+            int count=ps.executeUpdate();
+            if(count==1){return true;}
+            else{return false;}
+        }catch (Exception e){
+            System.out.println();
+        } return false;
     }
 }
