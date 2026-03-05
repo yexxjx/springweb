f09e1811f15465ef1d8e539ae886a063c3a40776
yexxjx <chenstyle02@gmail.com>
2026-03-05 18:18:57 +0900

    2600305 18:18

--- src/main/java/example/day03/practice3/AttendanceController.java
+++ src/main/java/example/day03/practice3/AttendanceController.java
@@ -1,11 +1,25 @@
 package example.day03.practice3;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping("/attendance")
 public class AttendanceController {
+    @PostMapping
+    public boolean 출석등록(@RequestBody AttendanceDto attendanceDto){
+        System.out.println("AttendanceController.출석등록");
+        System.out.println("attendanceDto = " + attendanceDto);
+        return true;
+    }
 
-
+    @GetMapping
+    public List<AttendanceDto> 출석전체조회(){
+        List<AttendanceDto> list=new ArrayList<>();
+        list.add(new AttendanceDto(1, "이연지", "2026-03-05", "출석"));
+        list.add(AttendanceDto.builder().status("출석").studentName("이연지").date("206-03-05").ano(1).build());
+        return list;
+    }
 }
