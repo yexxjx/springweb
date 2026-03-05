17374af900c9f8cd742343ac51f990461c24cb0a
yexxjx <chenstyle02@gmail.com>
2026-03-05 18:14:12 +0900

    2600305 18:14

--- src/main/java/example/day03/practice3/AttendanceController.java
+++ src/main/java/example/day03/practice3/AttendanceController.java
@@ -1,4 +1,11 @@
 package example.day03.practice3;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/attendance")
 public class AttendanceController {
+
+
 }
--- src/main/java/example/day03/practice3/AttendanceDto.java
+++ src/main/java/example/day03/practice3/AttendanceDto.java
@@ -1,4 +1,16 @@
 package example.day03.practice3;
 
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data @Builder
+@NoArgsConstructor @AllArgsConstructor
 public class AttendanceDto {
+    private Integer ano;
+    private String studentName;
+    private String date;
+    private String status;
 }
