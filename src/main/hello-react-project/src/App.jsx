ebc89a9e93fbaf1f3298e56ab4fa03178aabdc6b
yexxjx <chenstyle02@gmail.com>
2026-03-09 17:00:02 +0900

    260309 16:59

--- src/main/hello-react-project/src/App.jsx
+++ src/main/hello-react-project/src/App.jsx
@@ -1,8 +1,53 @@
-function App() {
+/* [1] 프론트 관련 컴포넌트 */
+function FrontComp(){ // 생성방법1: function 컴포넌트명(){
+  return(<>
+  <li>프론트엔드</li>
+    <ul>
+      <li>HTML5</li>
+      <li>CSS3</li>
+      <li>Javascript</li>
+      <li>jQuery</li>
+    </ul>
+  </>); // 생성방법2: 컴포넌트 내 return 뒤로 (<> HTML코드 </>)
+}
 
+/* [2] 백엔드 관련 컴포넌트 */
+const BackComp=()=>{
   return (<>
-  <h2>let's go</h2>
-  </>)
+  <li>백엔드</li>
+  <ul>
+    <li>Java</li>
+    <li>Oracle</li>
+    <li>JSP</li>
+    <li>Spring Boot</li>
+  </ul>  
+  </>);
+}
+/* [3] 폼 관련 컴포넌트 */
+let FormComp=function(){
+  return(<>
+  <form>
+    <select name="gubun">
+      <option value="front">프론트엔드</option>
+      <option value="back">백엔드</option>
+    </select>
+    <input type="text" name="title" />
+    <input type="submit" value="추가" />
+    </form>
+  </>);
+}
+
+/* [4] 여러 컴포넌트 호출/참조하는 컴포넌트 */
+function App() {
+  return (<>
+  <h2>React - 기본</h2>
+    <ol>
+      <FrontComp></FrontComp>
+      <BackComp/>
+      <BackComp/>
+    </ol>
+    <FormComp/>
+  </>);
 }
 
 export default App
--- src/main/hello-react-project/src/main.jsx
+++ src/main/hello-react-project/src/main.jsx
@@ -5,3 +5,12 @@ import App from './App.jsx'
 createRoot(document.getElementById('root')).render(
   <App />
 )
+
+
+/*
+[1] index.html(싱글페이지)에서 root라는 id갖는 div 호출
+const root= document.querySelector('#root');
+
+[2] root 마크업에 랜더링(render)
+createRoot(root).render(<h1> 안녕하세요! </h1>) VS root.innerHTML="<h1> 안녕하세요! </h1>";
+*/
