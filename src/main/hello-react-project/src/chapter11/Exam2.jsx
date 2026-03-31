ff48871a74fcaf341030a12d03aa38ca1eb58d4c
yexxjx <chenstyle02@gmail.com>
2026-03-31 14:49:40 +0900

    14:49

--- src/main/hello-react-project/src/chapter11/Exam2.jsx
+++ src/main/hello-react-project/src/chapter11/Exam2.jsx
@@ -12,15 +12,13 @@ export default function Exam2(props){
         <Routes> {/* 라우터들 설정 시작 */}
             <Route path='/' element={<Home/>} /> {/* 라우터 설정 */}
 
-        {/* 중첩 라우터 */}
+            {/* 중첩 라우터 */}
             <Route path='/intro' element={<CommonLayout/>}>
             {/* 경로가 별도로 없을 때 index 지정 */}
                 <Route index element={<LayoutIndex/>} />
-
             {/* 쿼리스트링 갖는 URL */}
                 <Route path='router' element={<RouterHooks/>} />
             </Route>
-
             <Route path='*' element={<NotFound/>} /> {/* 라우터 설정 */}
         </Routes>
     </>)
